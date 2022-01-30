be70357f4ea3499c99c8052e6ea90a6bf4120485
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-30 12:52:34 +0900

    feat: 🎸 ファーストリクエストからのレスポンス取得が完成

--- app.ts
+++ app.ts
@@ -2,43 +2,61 @@ import axios, { AxiosResponse, AxiosError } from 'axios'
 
 const baseUrl: string = 'https://vg-gamemedia-kamigame-enquete.appspot.com/comment/list?path=%2Farcalast%2F%25E6%258E%25B2%25E7%25A4%25BA%25E6%259D%25BF.html&no_pager='
 
+type ResponseData = {
+  comments: Array<{
+    id: string
+    nickname: string
+    body: string
+    image_url: string
+    comment_no: number
+    good_vote: number
+    bad_vote: number
+    created_at: string
+  }>
+  cursor: string
+}
+
 const firstGet: any = async () => {
   return axios.get(`${baseUrl}`)
     .then((response: AxiosResponse) => {
       console.log('初期データを取得')
 
-      return response.data.cursor
+      return response.data
     })
     .catch((error: AxiosError) => {
       console.log(error)
     })
-    .then((cursor: string) => {
+    .then((responseData: ResponseData) => {
       console.log('axios done')
-      console.log(`cursor: ${cursor}`)
+      console.log(`cursor: ${responseData.cursor}`)
 
-      return cursor
+      return responseData
     })
 }
 
-const secondGet = async (cursor: string | null) => {
-  axios.get(`${baseUrl}${cursor}`)
-    .then((response: AxiosResponse) => {
-      return response.data.cursor
-    })
-    .catch((error: AxiosError) => {
-      console.log(error)
-    })
-    .then((cursor: string) => {
-      return cursor
-    })
-}
+// const secondGet = async (cursor: string | null) => {
+//   return axios.get(`${baseUrl}false&cursor=${cursor}`)
+//     .then((response: AxiosResponse) => {
+//       return response.data.cursor
+//     })
+//     .catch((error: AxiosError) => {
+//       console.log(error)
+//     })
+//     .then((cursor: string) => {
+//       return cursor
+//     })
+// }
 
 const allGet: any = async () => {
-  const cursor: string = await firstGet()
-  console.log(cursor)
+  const responseData: ResponseData = await firstGet()
 
-  const secondCursor = await secondGet(cursor)
-  console.log(secondCursor)
+  console.log('========== 1st comment ==========')
+  console.log(responseData.comments[0])
+  console.log('========== 1st cursor ==========')
+  console.log(responseData.cursor)
+
+  // const secondCursor = await secondGet(cursor)
+  // console.log(secondCursor)
 
   // firstGet().then(value => {
   //   console.log(value)
