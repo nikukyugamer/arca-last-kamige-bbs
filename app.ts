a2085c3b7d68880826903f5ed2c43f6c7fb6d095
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-30 13:17:10 +0900

    refactor: 💡 単一の関数に収めた

--- app.ts
+++ app.ts
@@ -1,7 +1,5 @@
 import axios, { AxiosResponse, AxiosError } from 'axios'
 
-const baseUrl: string = 'https://vg-gamemedia-kamigame-enquete.appspot.com/comment/list?path=%2Farcalast%2F%25E6%258E%25B2%25E7%25A4%25BA%25E6%259D%25BF.html&no_pager='
-
 type ResponseData = {
   comments: Array<{
     id: string
@@ -16,54 +14,42 @@ type ResponseData = {
   cursor: string
 }
 
-const firstGet: any = async () => {
-  return axios.get(`${baseUrl}`)
-    .then((response: AxiosResponse) => {
-      console.log('初期データを取得')
+const apiResponseData: any = async (cursor: string | null) => {
+  let requestUrl = 'https://vg-gamemedia-kamigame-enquete.appspot.com/comment/list?path=%2Farcalast%2F%25E6%258E%25B2%25E7%25A4%25BA%25E6%259D%25BF.html&no_pager='
 
+  if (cursor !== null) {
+    requestUrl = `${requestUrl}false&cursor=${cursor}`
+  }
+
+  return axios.get(requestUrl)
+    .then((response: AxiosResponse) => {
       return response.data
     })
     .catch((error: AxiosError) => {
       console.log(error)
     })
     .then((responseData: ResponseData) => {
-      console.log('axios done')
-      console.log(`cursor: ${responseData.cursor}`)
-
       return responseData
     })
 }
 
-// const secondGet = async (cursor: string | null) => {
-//   return axios.get(`${baseUrl}false&cursor=${cursor}`)
-//     .then((response: AxiosResponse) => {
-//       return response.data.cursor
-//     })
-//     .catch((error: AxiosError) => {
-//       console.log(error)
-//     })
-//     .then((cursor: string) => {
-//       return cursor
-//     })
-// }
-
 const allGet: any = async () => {
-  const responseData: ResponseData = await firstGet()
+  const responseData: ResponseData = await apiResponseData(null)
 
   console.log('========== 1st comment ==========')
   console.log(responseData.comments[0])
-  console.log('========== 1st cursor ==========')
-  console.log(responseData.cursor)
 
-  // const secondCursor = await secondGet(cursor)
-  // console.log(secondCursor)
+  let latestCursor = responseData.cursor
 
-  // firstGet().then(value => {
-  //   console.log(value)
-  // })
-  // const hoge = await firstGet()
-  // firstGet().then(secondGet)
+  for (let i = 2; i < 5; i++) {
+    const latestResponseData = await apiResponseData(latestCursor)
+
+    console.log(`========== ${i}th comment ==========`)
+    console.log(latestResponseData.comments[0])
+
+    latestCursor = latestResponseData.cursor
+  }
 }
 
-// firstGet()
+// created_at は例えば Ruby だと Time.parse(t += 'JST') で Timeクラス として取得できる
 allGet()
