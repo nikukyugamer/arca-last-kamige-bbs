6fed977c75d209da251cc36fd50975179e26e076
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-30 12:44:28 +0900

    feat: 🎸 cursorを取得

--- app.ts
+++ app.ts
@@ -1,19 +1,51 @@
 import axios, { AxiosResponse, AxiosError } from 'axios'
 
-const baseUrl = 'https://vg-gamemedia-kamigame-enquete.appspot.com/comment/list?path=%2Farcalast%2F%25E6%258E%25B2%25E7%25A4%25BA%25E6%259D%25BF.html&no_pager='
+const baseUrl: string = 'https://vg-gamemedia-kamigame-enquete.appspot.com/comment/list?path=%2Farcalast%2F%25E6%258E%25B2%25E7%25A4%25BA%25E6%259D%25BF.html&no_pager='
 
-const firstGet = async () => {
-  axios.get(`${baseUrl}`)
-    .then(function (response: AxiosResponse) {
+const firstGet: any = async () => {
+  return axios.get(`${baseUrl}`)
+    .then((response: AxiosResponse) => {
       console.log('初期データを取得')
-      console.log(response.data)
+
+      return response.data.cursor
     })
-    .catch(function (error: AxiosError) {
+    .catch((error: AxiosError) => {
       console.log(error)
     })
-    .then(function () {
+    .then((cursor: string) => {
       console.log('axios done')
+      console.log(`cursor: ${cursor}`)
+
+      return cursor
     })
 }
 
-firstGet()
+const secondGet = async (cursor: string | null) => {
+  axios.get(`${baseUrl}${cursor}`)
+    .then((response: AxiosResponse) => {
+      return response.data.cursor
+    })
+    .catch((error: AxiosError) => {
+      console.log(error)
+    })
+    .then((cursor: string) => {
+      return cursor
+    })
+}
+
+const allGet: any = async () => {
+  const cursor: string = await firstGet()
+  console.log(cursor)
+
+  const secondCursor = await secondGet(cursor)
+  console.log(secondCursor)
+
+  // firstGet().then(value => {
+  //   console.log(value)
+  // })
+  // const hoge = await firstGet()
+  // firstGet().then(secondGet)
+}
+
+// firstGet()
+allGet()
